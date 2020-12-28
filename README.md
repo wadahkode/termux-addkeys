f4bdb0520858aab826f3f18fe0eed5ed529356cf
apolbox <ayus.sahabat@gmail.com>
2020-12-28 11:41:24 +0700

    update

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Menampillkan semua tombol dari keyboard
 	$ apt install git
 	$ git clone https://github.com/apolbox/termux-addkeys.git
 	$ cd termux-addkeys
+	$ chmod +x ./install.sh
 	$ ./install.sh
 
 Selesai.
---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# Tombol tambahan untuk termux
+# @author Ayus Irfang Filaras <ayus.sahabat@gmail.com>
+# license MIT
+clear
+# Sambutan
+printf "\e[32;m📲 Tombol tambahan untuk termux\e[0m\n"
+sleep 1
+
+# extra-keys
+key="extra-keys = [['ESC','/','-','HOME','UP','END','PGUP'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT','PGDN']]"
+
+# buka properties
+prop="$HOME/.termux/termux.properties"
+
+if [[ -f $prop ]]; then
+  rm $prop
+  echo "${key}" >> $prop
+  sleep 1
+  termux-reload-settings
+  printf "\e[32;m\n\n[√] Berhasil menambahkan tombol tambahan pada termux.\e[0m\n"
+
+else
+  echo "{$key}" >> $prop
+  sleep 1
+  termux-reload-settings
+  
+  printf "\e[32;m\n\n[√] Berhasil menambahkan tombol tambahan pada termux.\e[0m\n"
+fi
